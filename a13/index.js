0696cf03234076970768df2981fe42cd386c719d
shruthi-reddy14 <amireddyshruthi14@gmail.com>
2025-05-16 20:19:19 +0530

    updated

---
+++ a13/index.js
@@ -0,0 +1,15 @@
+const products=[
+  {id:1,name:"p1",price:35},
+{id:2,name:"p2",price:30},
+{id:3,name:"p3",price:40}
+];
+
+let news = products.map((v)=>v.price+5)
+console.log(news)
+
+news.forEach
+
+
+
+
+
---
+++ a14/index.js
@@ -0,0 +1,2 @@
+fetch("https://jsonplaceholder.typicode.com/posts").then((res)=>
+res.json().then((data)=>console.log(data)));
