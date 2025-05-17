bb498b3e9158cf65488fff3817d9fefd5c158ae0
shruthi-reddy14 <amireddyshruthi14@gmail.com>
2025-05-17 19:00:54 +0530

    updated

--- a13/index.js
+++ a13/index.js
@@ -1,11 +1,14 @@
 const products=[
-  {id:1,name:"p1",price:35},
-{id:2,name:"p2",price:30},
-{id:3,name:"p3",price:40}
+  {id:1,name:"p1",price:35,status:"pending",qty:2},
+{id:2,name:"p2",price:30,status:"pending",qty:5},
+{id:3,name:"p3",price:40,status:"pending",qty:3}
 ];
 
-let newPrice = products.map((v)=>v.price+5)
- products.forEach((ele)=>console.log(ele.name+" "+newPrice))
+let newPrice = products.map((v)=>({...v,price:v.price+5,status:"complete",total:v.qty*v.price}));
+
+
+newPrice.forEach((ele)=>console.log(ele.status+" "+ele.price+" "+ele.qty+" "+ele.total))
+
 
 
 
