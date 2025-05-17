c43d3dc2b7f7039f726151f5fd8895cd240da2c6
shruthi-reddy14 <amireddyshruthi14@gmail.com>
2025-05-17 19:45:52 +0530

    updatred

--- a13/index.js
+++ a13/index.js
@@ -1,15 +1,13 @@
-// const products=[
-//   {id:1,name:"p1",price:35,status:"pending",qty:2},
-// {id:2,name:"p2",price:30,status:"pending",qty:5},
-// {id:3,name:"p3",price:40,status:"pending",qty:3}
-// ];
+const products=[
+ {id:1,name:"p1",price:35,status:"pending",qty:2},
+{id:2,name:"p2",price:30,status:"pending",qty:5},
+ {id:3,name:"p3",price:40,status:"pending",qty:3}
+ ];
 
-// // let newPrice = products.map((v)=>({...v,price:v.price+5,status:"complete",total:v.qty*v.price}));
+//  let newPrice = products.map((v)=>({...v,price:v.price+5,status:"complete",total:v.qty*v.price}));
 
-// let newPrice=products.filter((v)=>v.qty >2).map((v)=>({...v,total:v.qty*v.price}))
-
-
-// newPrice.forEach((ele)=>console.log(ele.status+" "+ele.price+" "+ele.qty+" "+ele.total))
+let newPrice=products.filter((v)=>v.qty >2).map((v)=>({...v,total:v.qty*v.price}))
+newPrice.forEach((ele)=>console.log(ele.status+" "+ele.price+" "+ele.qty+" "+ele.total))
 
 
 
